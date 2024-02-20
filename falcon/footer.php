57d1e302c40f9b90ecf51d17035b77cda5ef95d2
irukaerika <41093544+irukaerika@users.noreply.github.com>
2024-02-20 21:46:27 +0900

    Add files via upload

---
+++ falcon/footer.php
@@ -0,0 +1,22 @@
+<footer class="footer_nav" id="flexBox">
+        <nav id="nav-pc">
+          <a href="#">HOME</a>
+          <a href="#">SERVICE</a>
+          <a href="#">STOCK LIST</a>
+          <a href="#">NEWS</a>
+          <a href="#">BLOG</a>
+          <a href="#">SHOP</a>
+          <a href="#">DOWNLOAD</a>
+        </nav>
+      <div class="copyrght">
+        <p>©️ 2021 Design Plus Inc.</p>
+      </div>
+    </div>
+  </footer>
+  <script src="https://code.jquery.com/jquery-3.6.0.min.js" 
+  integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4="
+  crossorigin="anonymous">
+  </script>
+  <script type="text/javascript" src="<?php get_template_directory_uri(); ?>/js/carousel.js"></script>
+</body>
+</html>
---
+++ falcon/header.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="<?php get_template_directory_uri(); ?>/styles/main_styles.css">
+  <title>Document</title>
+  <?php wp_head(); ?>
+</head>
+<body>
+  <header id="flexBox" class="header">
+    <a href="<?php get_template_directory_uri(); ?>/home.php">
+      <div>FALCON</div>
+    </a>
+    <nav id="nav-pc">
+      <a href="#">HOME</a>
+      <a href="#">SERVICE</a>
+      <a href="#">STOCK LIST</a>
+      <a href="#">NEWS</a>
+      <a href="#">BLOG</a>
+      <a href="#">SHOP</a>
+      <a href="#">DOWNLOAD</a>
+    </nav>
+      <a href="#">検索マーク</a>
+  </header>
