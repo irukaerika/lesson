6bd85965d8651afdc63b17856a2e1f9465568679
irukaerika <41093544+irukaerika@users.noreply.github.com>
2024-02-22 00:18:13 +0900

    Add files via upload

--- falcon/footer.php
+++ falcon/footer.php
@@ -1,22 +1,21 @@
-<footer class="footer_nav" id="flexBox">
-        <nav id="nav-pc">
-          <a href="#">HOME</a>
-          <a href="#">SERVICE</a>
-          <a href="#">STOCK LIST</a>
-          <a href="#">NEWS</a>
-          <a href="#">BLOG</a>
-          <a href="#">SHOP</a>
-          <a href="#">DOWNLOAD</a>
-        </nav>
-      <div class="copyrght">
-        <p>©️ 2021 Design Plus Inc.</p>
-      </div>
+  <footer class="footer_nav" id="flexBox">
+    <nav id="nav-pc">
+      <a href="<?php echo home_url(); ?>">HOME</a>
+      <a href="<?php echo home_url(); ?>/category/service">SERVICE</a>
+      <a href="<?php echo home_url(); ?>/category/stock_list">STOCK LIST</a>
+      <a href="<?php echo home_url(); ?>/category/news">NEWS</a>
+      <a href="<?php echo home_url(); ?>/category/blog">BLOG</a>
+      <a href="<?php echo home_url(); ?>/category/shop">SHOP</a>
+      <a href="<?php echo home_url(); ?>/category/download">DOWNLOAD</a>
+    </nav>
+    <div class="copyrght">
+      <p>©️ 2021 Design Plus Inc.</p>
     </div>
   </footer>
   <script src="https://code.jquery.com/jquery-3.6.0.min.js" 
   integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4="
   crossorigin="anonymous">
   </script>
-  <script type="text/javascript" src="<?php get_template_directory_uri(); ?>/js/carousel.js"></script>
+  <script type="text/javascript" src="<?php echo get_template_directory_uri(); ?>/js/carousel.js"></script>
 </body>
 </html>
