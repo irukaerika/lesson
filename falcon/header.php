2c56f0919c006575ee7823655851c402178f0cf7
irukaerika <41093544+irukaerika@users.noreply.github.com>
2024-02-22 00:17:36 +0900

    Add files via upload

--- falcon/header.php
+++ falcon/header.php
@@ -1,25 +1,27 @@
 <!DOCTYPE html>
 <html lang="ja">
 <head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="stylesheet" href="<?php get_template_directory_uri(); ?>/styles/main_styles.css">
+  <meta charset="UTF-8"/>
+  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+  <meta name="description" content="samurai university" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+  <link rel="stylesheet"  type="text/css" href="<?php echo get_template_directory_uri(); ?>/styles/main_styles.css"/>
   <title>Document</title>
   <?php wp_head(); ?>
 </head>
 <body>
   <header id="flexBox" class="header">
-    <a href="<?php get_template_directory_uri(); ?>/home.php">
+    <a href="<?php echo get_template_directory_uri(); ?>/home.php">
       <div>FALCON</div>
     </a>
     <nav id="nav-pc">
-      <a href="#">HOME</a>
-      <a href="#">SERVICE</a>
-      <a href="#">STOCK LIST</a>
-      <a href="#">NEWS</a>
-      <a href="#">BLOG</a>
-      <a href="#">SHOP</a>
-      <a href="#">DOWNLOAD</a>
+      <a href="<?php echo home_url(); ?>">HOME</a>
+      <a href="<?php echo home_url(); ?>/category/service">SERVICE</a>
+      <a href="<?php echo home_url(); ?>/category/stock_list">STOCK LIST</a>
+      <a href="<?php echo home_url(); ?>/category/news">NEWS</a>
+      <a href="<?php echo home_url(); ?>/category/blog">BLOG</a>
+      <a href="<?php echo home_url(); ?>/category/shop">SHOP</a>
+      <a href="<?php echo home_url(); ?>/category/download">DOWNLOAD</a>
     </nav>
       <a href="#">検索マーク</a>
   </header>
